52d061e87958c05b1ce5b9efbc9465ed5dc88dc8
aracelymejia <52581612+aracelymejia@users.noreply.github.com>
2019-12-05 20:42:11 -0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -71,6 +71,7 @@
 ;*******************************************************************************
 
 ; TODO INSERT INCLUDE CODE HERE
+#include <p18f46k22.inc>
 
 ;*******************************************************************************
 ;
@@ -171,8 +172,6 @@ RES_VECT  CODE    0x0000            ; processor reset vector
 ;
 ;*******************************************************************************
 
-; TODO INSERT ISR HERE
-
 ;*******************************************************************************
 ; MAIN PROGRAM
 ;*******************************************************************************
@@ -180,389 +179,11 @@ RES_VECT  CODE    0x0000            ; processor reset vector
 MAIN_PROG CODE                      ; let linker place main program
 
 START
-;    ; test 1
-;    ; initialize code  0123
-;    MOVLW H'01'
-;    MOVWF 0x004
-;    MOVLW H'23'
-;    MOVWF 0x005
-;    ; initialize guess 1023
-;    MOVLW H'10'
-;    MOVWF 0x010
-;    MOVLW H'23'
-;    MOVWF 0x011
-;    ; B = 2 W = 2
-
-;    ; test 2
-;    ; initialize code  0111
-;    MOVLW H'01'
-;    MOVWF 0x004
-;    MOVLW H'11'
-;    MOVWF 0x005
-;    ; initialize guess 1000
-;    MOVLW H'10'
-;    MOVWF 0x010
-;    MOVLW H'00'
-;    MOVWF 0x011
-;    ; B = 0 W = 2
-
-;    ; test 3
-;    ; initialize code  5411
-;    MOVLW H'54'
-;    MOVWF 0x004
-;    MOVLW H'11'
-;    MOVWF 0x005
-;    ; initialize guess 5114
-;    MOVLW H'51'
-;    MOVWF 0x010
-;    MOVLW H'14'
-;    MOVWF 0x011
-;    ; B = 2 W = 2
-
-;    ; test 4
-;    ; initialize code  0133
-;    MOVLW H'01'
-;    MOVWF 0x004
-;    MOVLW H'33'
-;    MOVWF 0x005
-;    ; initialize guess 3013
-;    MOVLW H'30'
-;    MOVWF 0x010
-;    MOVLW H'13'
-;    MOVWF 0x011
-;    ; B = 1 W = 3
-
-#include <p18f46k22.inc>
-
-GOTO Main
  
-Main:
-    ;set up counters
-    Count0 EQU 0xE0
-    Count1 EQU 0xE1
-    Count2 EQU 0xE2 ;counter for delays
- 
-    ;here will be setting up the ports for the physical board - PORTA, PORTD, LCD
-    ;calling our other subroutines
-    ;using temp wr and rd
-    ;CPURandom1 EQU 0xC0
-    ;CPURandom2 EQU 0xC1
-    ;CPURandom3 EQU 0xC2
-
-RNG
-    CPURAND0 EQU 0x0
-    CPURAND1 EQU 0x1
-    CPURAND2 EQU 0x2
- 
-    ;initialize counters to zero
-    MOVLW 0H
-    MOVWF CPURAND0
-    MOVLW 0H
-    MOVWF CPURAND1
-    MOVLW 0H
-    MOVWF CPURAND2
-    
-    ;downward count
-    MOVLW 5H
-    MOVWF 0xD1
-    MOVLW 5H
-    MOVWF 0xD2
-    MOVLW 5H
-    MOVWF 0xD3
-    
-    ;begin the random number generator for play to guess
-    ;register to register counts
-    RCOUNT1 MOVLW 0H
-	   MOVWF CPURAND1
-    RCOUNT2 MOVLW 0H
-	   MOVWF CPURAND2
-	   MOVLW 5H
-	   MOVWF 0xD3
-    RCOUNT3 INCF CPURAND2
-	   DECFSZ 0xD3		;skip next instruction if zero
-	   BRA RCOUNT3		;jump back to top
-	   INCF CPURAND1	;increase register
-	   DECFSZ 0xD2		;skip next instruction if zero
-	   BRA RCOUNT2		;jump back to register that holds count 2
-	   INCF CPURAND0	
-	   DECFSZ 0xD1		
-	   BRA RCOUNT1
-	   MOVLW 0H
-	   MOVWF CPURAND0 
-	   MOVLW 5H
-	   MOVWF 0xD3
- 
-    CODE0 EQU 0x004
-    CODE1 EQU 0x005
- 
-    GUESS0 EQU 0x010
-    GUESS1 EQU 0x011
-
-FEEDBACK
-    ; unpack the guess into individual registers 
-    
-    ; unpacked code vars
-    COLOR0 EQU 0x020
-    COLOR1 EQU 0x021
-    COLOR2 EQU 0x022
-    COLOR3 EQU 0x023
-    
-    ; unpacked guess vars
-    FB0 EQU 0x024
-    FB1 EQU 0x025
-    FB2 EQU 0x026
-    FB3 EQU 0x027
-    
-    ; unpack first bype of code/guess
-    MOVFF CODE0, COLOR0
-    MOVFF GUESS0, FB0    
-    MOVLW H'F0'
-    ANDWF COLOR0, F
-    RRCF COLOR0, F
-    RRCF COLOR0, F
-    RRCF COLOR0, F
-    RRCF COLOR0, F
-    ANDWF FB0, F
-    RRCF FB0, F
-    RRCF FB0, F
-    RRCF FB0, F
-    RRCF FB0, F
-    
-    ; unpack second bype of code/guess
-    MOVFF CODE0, COLOR1
-    MOVFF GUESS0, FB1  
-    MOVLW H'0F'
-    ANDWF COLOR1, F
-    ANDWF FB1, F
-    
-    ; unpack third bype of code/guess
-    MOVFF CODE1, COLOR2
-    MOVFF GUESS1, FB2
-    MOVLW H'F0'
-    ANDWF COLOR2, F
-    RRCF COLOR2, F
-    RRCF COLOR2, F
-    RRCF COLOR2, F
-    RRCF COLOR2, F
-    ANDWF FB2, F
-    RRCF FB2, F
-    RRCF FB2, F
-    RRCF FB2, F
-    RRCF FB2, F
-    
-    ; unpack forth bype of code/guess
-    MOVFF CODE1, COLOR3
-    MOVFF GUESS1, FB3  
-    MOVLW H'0F'
-    ANDWF COLOR3, F
-    ANDWF FB3, F
-    
-    ; initialize the blacks and whites
-    BLACKS EQU 0x030
-    WHITES EQU 0x031
-    CLRF BLACKS
-    CLRF WHITES
-   
-; count the blacks
-;   checking if (COLOR == FB)
-;   if so, increment blacks and mark both COLOR and FB with a flag
-;   by setting bit 7 of both to high, so we don't count it as a white
-BLACK_CHECK3
-    MOVF FB3, W
-    CPFSEQ COLOR3
-    GOTO BLACK_CHECK2
-    INCF BLACKS
-    BSF FB3, 7
-    BSF COLOR3, 7
-    
-BLACK_CHECK2
-    MOVF FB2, W
-    CPFSEQ COLOR2
-    GOTO BLACK_CHECK1
-    INCF BLACKS
-    BSF FB2, 7
-    BSF COLOR2, 7
-    
-BLACK_CHECK1
-    MOVF FB1, W
-    CPFSEQ COLOR1
-    GOTO BLACK_CHECK0
-    INCF BLACKS
-    BSF FB1, 7
-    BSF COLOR1, 7
-    
-BLACK_CHECK0
-    MOVF FB0, W
-    CPFSEQ COLOR0
-    GOTO WHITE_CHECK3
-    INCF BLACKS
-    BSF FB0, 7
-    BSF COLOR0, 7
-
-; count the whites
-;   check if FB matches a COLOR that doesn't already have a flag and 
-;   doesn't share the same location (if it did, it would be a black)
-;   if so, increment whites and mark FB and COLOR with a flag
-;   else, check if FB matches any other color using the orignial check condition
-WHITE_CHECK3
-    ; compare FB3 with COLOR2, COLOR1, and COLOR0
-    ; if FB is flagged, it is marked as a black, so go to the next FB
-    BTFSC FB3, 7
-    GOTO WHITE_CHECK2
-    
-    MOVF FB3, W
-    
-    ; if COLOR is flagged, check the next color
-    BTFSC COLOR2, 7
-    GOTO WHITE_CHECK3_1
-    
-    ; if FB == COLOR, increment white, flag COLOR, and check next FB
-    CPFSEQ COLOR2
-    GOTO WHITE_CHECK3_1
-    INCF WHITES
-    BSF COLOR2, 7
-    GOTO WHITE_CHECK2
-    
-WHITE_CHECK3_1
-    BTFSC COLOR1, 7
-    GOTO WHITE_CHECK3_0
-    
-    CPFSEQ COLOR1
-    GOTO WHITE_CHECK3_0
-    INCF WHITES
-    BSF COLOR1, 7
-    GOTO WHITE_CHECK2
-
-WHITE_CHECK3_0
-    BTFSC COLOR0, 7
-    GOTO WHITE_CHECK2
-    
-    CPFSEQ COLOR0
-    GOTO WHITE_CHECK2
-    INCF WHITES
-    BSF COLOR0, 7
-    
-; --------
-WHITE_CHECK2
-    ; compare FB2 with COLOR3, COLOR1, and COLOR0
-    ; if FB is flagged, it is marked as a black, so go to the next FB
-    BTFSC FB2, 7
-    GOTO WHITE_CHECK1
-    
-    MOVF FB2, W
-    
-    ; if COLOR is flagged, check the next color
-    BTFSC COLOR3, 7
-    GOTO WHITE_CHECK2_1
-    
-    ; if FB == COLOR, increment white, flag COLOR, and check next FB
-    CPFSEQ COLOR3
-    GOTO WHITE_CHECK2_1
-    INCF WHITES
-    BSF COLOR3, 7
-    GOTO WHITE_CHECK1
-    
-WHITE_CHECK2_1
-    BTFSC COLOR1, 7
-    GOTO WHITE_CHECK2_0
-    
-    CPFSEQ COLOR1
-    GOTO WHITE_CHECK2_0
-    INCF WHITES
-    BSF COLOR1, 7
-    GOTO WHITE_CHECK1
-
-WHITE_CHECK2_0
-    BTFSC COLOR0, 7
-    GOTO WHITE_CHECK1
-    
-    CPFSEQ COLOR0
-    GOTO WHITE_CHECK1
-    INCF WHITES
-    BSF COLOR0, 7
-   
-; --------
-WHITE_CHECK1
-    ; compare FB1 with COLOR3, COLOR2, and COLOR0
-    ; if FB is flagged, it is marked as a black, so go to the next FB
-    BTFSC FB1, 7
-    GOTO WHITE_CHECK0
-    
-    MOVF FB1, W
-    
-    ; if COLOR is flagged, check the next color
-    BTFSC COLOR3, 7
-    GOTO WHITE_CHECK1_2
-    
-    ; if FB == COLOR, increment white, flag COLOR, and check next FB
-    CPFSEQ COLOR3
-    GOTO WHITE_CHECK1_2
-    INCF WHITES
-    BSF COLOR3, 7
-    GOTO WHITE_CHECK0
-    
-WHITE_CHECK1_2
-    BTFSC COLOR2, 7
-    GOTO WHITE_CHECK1_0
-    
-    CPFSEQ COLOR2
-    GOTO WHITE_CHECK1_0
-    INCF WHITES
-    BSF COLOR2, 7
-    GOTO WHITE_CHECK0
-
-WHITE_CHECK1_0
-    BTFSC COLOR0, 7
-    GOTO WHITE_CHECK0
-    
-    CPFSEQ COLOR0
-    GOTO WHITE_CHECK0
-    INCF WHITES
-    BSF COLOR0, 7
-    
-; --------
-WHITE_CHECK0
-    ; compare FB0 with COLOR3, COLOR2, and COLOR1
-    ; if FB is flagged, it is marked as a black, so go to the next FB
-    BTFSC FB0, 7
-    GOTO FEEDBACK_END
-    
-    MOVF FB0, W
-    
-    ; if COLOR is flagged, check the next color
-    BTFSC COLOR3, 7
-    GOTO WHITE_CHECK0_2
-    
-    ; if FB == COLOR, increment white, flag COLOR, and check next FB
-    CPFSEQ COLOR3
-    GOTO WHITE_CHECK0_2
-    INCF WHITES
-    BSF COLOR3, 7
-    GOTO FEEDBACK_END
-    
-WHITE_CHECK0_2
-    BTFSC COLOR2, 7
-    GOTO WHITE_CHECK0_1
-    
-    CPFSEQ COLOR2
-    GOTO WHITE_CHECK0_1
-    INCF WHITES
-    BSF COLOR2, 7
-    GOTO FEEDBACK_END
-
-WHITE_CHECK0_1
-    BTFSC COLOR1, 7
-    GOTO FEEDBACK_END
-    
-    CPFSEQ COLOR1
-    GOTO FEEDBACK_END
-    INCF WHITES
-    BSF COLOR1, 7
-    
-FEEDBACK_END
- 
- ;********************Aracely's code below:
- #include <p18f46k22.inc>
+; set up LCD screen
+Count0 EQU 0xE0
+Count1 EQU 0xE1
+Count2 EQU 0xE2 ;counter for delays
 COUNTER	EQU 0xE3	;counter for delay loops
 delay EQU 0xE4		;amount  of  delay for delay subroutines
 temp_wr EQU 0xE5	;temporary register for LCD written data
@@ -571,22 +192,28 @@ ptr_pos EQU 0xE7
 ptr_count EQU 0xE8
 cmd_byte EQU 0xE9	;used by LCD routines 
 
-    GOTO Main
- 
+    GoTo	Main
+
 Main:
-;PORTA EQU 0xF60	;set port location 
-    BSF TRISA,4		;sets PORTA bit 4 as an input -- SW2
- 
-    ;set up counters
-Count0 EQU 0xE0
-Count1 EQU 0xE1
-Count2 EQU 0xE2 ;counter for delays
- 
-    ;here will be setting up the ports for the physical board - PORTA, LCD
-    call	LCDInit		;Initialize PORTA and LCD Module
+    BSF TRISA,4			; sets PORTA bit 4 as an input -- SW2
+    call	LCDInit		; Initialize PORTA, PORTD, and LCD Module
     call	LCDLine_1	;move cursor to line 1
     
-Display:    
+; ---------------- Wait a while = 1 x256 x256 x3 uSec = 0.1 sec
+    movLW	1
+    movWF	Count2
+    clrF	Count1
+    clrF	Count0
+WLoo	
+    decFsZ	Count0
+    bra		WLoo
+    decFsZ	Count1
+    bra		WLoo
+    decFsZ	Count2
+    bra		WLoo	
+    
+Display:
+    ; print welcome message
     call	LCDLine_1
     movLW	A'W'
     movWF	temp_wr
@@ -607,7 +234,9 @@ Display:
     movWF	temp_wr
     call	d_write
     movLW	A'E'
-	
+    movWF	temp_wr
+    call	d_write
+    
     call	LCDLine_2
     movLW	A'S'
     movWF	temp_wr
@@ -637,76 +266,378 @@ Display:
     movWF	temp_wr
     call	d_write
     
-    NOP ;cycle
-    NOP
-    NOP
-    
-    
-;***********RNG
-startWait
-   
-CPURAND0 EQU 0x0
-CPURAND1 EQU 0x1
-CPURAND2 EQU 0x2
-CPURAND3 EQU 0x3
+    call RNG
  
-    ;initialize counters to zero
-    MOVLW 0H
-    MOVWF CPURAND0
-    MOVLW 0H
-    MOVWF CPURAND1
-    MOVLW 0H
-    MOVWF CPURAND2
-    MOVLW 0H
-    MOVWF CPURAND3
-    
-    ;downward count
-    MOVLW 5H
-    MOVWF 0xD1
-    MOVLW 5H
-    MOVWF 0xD2
-    MOVLW 5H
-    MOVWF 0xD3
-    
-    BTFSS PORTA,4		
-    BRA startProgram	
-    
-    ;begin the random number generator for play to guess
-    ;register to register counts
-RCOUNT1 MOVLW 0H
-	   MOVWF CPURAND1
-RCOUNT2 MOVLW 0H
-	   MOVWF CPURAND2
-	   MOVLW 5H
-	   MOVWF 0xD3
-RCOUNT3 INCF CPURAND2
-	   BTFSS PORTA,4	;if button is pushed
-	   DECFSZ 0xD3		;skip next instruction if zero
-	   BRA RCOUNT3		;jump back to top
-	   INCF CPURAND1	;increase register
-	   BTFSS PORTA,4	
-	   DECFSZ 0xD2		;skip next instruction if zero
-	   BRA RCOUNT2		;jump back to register that holds count 2
-	   INCF CPURAND0
-	   BTFSS PORTA,4	
-	   DECFSZ 0xD1		
-	   BRA RCOUNT1
-	   DECFSZ CPURAND0
-	  
-    GOTO startWait
-    
-startProgram    
-    BTFSS	PORTA,4 ;wait for SW2 to be pressed
-    BRA		$-2	;wait for SW2 release	
+CODE_PACK1 EQU 0x500
+CODE_PACK2 EQU 0x501
 
+CODE1 EQU 0x510
+CODE2 EQU 0x511
+CODE3 EQU 0x512
+CODE4 EQU 0X513
 
-	
-#Include LCD_p18LCD_Subs_New.asm  ; Contains:
+; TODO: configure the ports
+ 
+; TODO: welcome message
+ 
+; TODO: rng loop
+ 
+; TODO: store code
+ 
+UNPACK_CODE
+    MOVFF CODE_PACK1, 0xA10
+    CALL UNPACK
+    MOVFF 0xA00, CODE1
+    MOVFF 0xA01, CODE2
+    
+    MOVFF CODE_PACK2, 0xA10
+    CALL UNPACK
+    MOVFF 0xA00, CODE3
+    MOVFF 0xA01, CODE4
+    
+    MOVFF CODE1, 0xA00
+    MOVFF CODE2, 0xA01
+    MOVFF CODE3, 0xA02
+    MOVFF CODE4, 0xA03
+
+GUESS_PACK1 EQU 0x600
+GUESS_PACK2 EQU 0x601
+ 
+GUESS1 EQU 0x610
+GUESS2 EQU 0x611
+GUESS3 EQU 0x612
+GUESS4 EQU 0x613
+ 
+GUESS_PACK_ARRAY EQU 0x620
+BLK_CNT_ARRAY EQU 0x640
+WT_CNT_ARRAY EQU 0x650
+    
+    LFSR 0, GUESS_PACK_ARRAY
+    LFSR 1, BLK_CNT_ARRAY
+    LFSR 2, WT_CNT_ARRAY
+ 
+GUESS_NUM EQU 0x660
+    MOVLB 0x6
+    CLRF GUESS_NUM
+    
+GUESS_LOOP
+    ; TODO: prompt for guess
+    
+    ; unpack the guess
+    MOVFF GUESS_PACK1, 0xA10
+    CALL UNPACK
+    MOVFF 0xA00, GUESS1
+    MOVFF 0xA01, GUESS2
+    
+    MOVFF GUESS_PACK2, 0xA10
+    CALL UNPACK
+    MOVFF 0xA00, GUESS3
+    MOVFF 0xA01, GUESS4
+    
+    ; determine blacks and whites
+    MOVFF CODE1, 0xA00
+    MOVFF CODE2, 0xA01
+    MOVFF CODE3, 0xA02
+    MOVFF CODE4, 0xA03
+    MOVFF GUESS1, 0xA04
+    MOVFF GUESS2, 0xA05
+    MOVFF GUESS3, 0xA06
+    MOVFF GUESS4, 0xA07
+    CALL BLK_CNT
+    
+    ; store blacks
+    MOVFF 0xA10, INDF1
+    INCF FSR1
+    
+    CALL WT_CNT
+    
+    ; store whites
+    MOVFF 0xA10, INDF2
+    INCF FSR2
+        
+    ; TODO: display feedback
+    
+    ; TOOD: check for win condition
+    
+    ; pack and store the guess
+    MOVFF GUESS1, 0xA00
+    MOVFF GUESS2, 0xA01
+    CALL PACK
+    
+    MOVFF 0xA10, INDF0
+    INCF INDF0
+   
+    MOVFF GUESS3, 0xA00
+    MOVFF GUESS4, 0xA01
+    CALL PACK
+    
+    MOVFF 0xA10, INDF0
+    INCF INDF0
+    
+    MOVLB 0x6
+    INCF GUESS_NUM, 1
+    
+    GOTO GUESS_LOOP
+
+;*******************************************************************************
+; SUBROUTINES
+;*******************************************************************************
+
+PACK
+    ; Reads 2 single digits stored in two 8-bit registers, and packs them
+    ; into one 8-bit register.
+    ;
+    ; Input at 0x0A00 (r1) and 0x0A01 (r2)
+    ; Output at 0x0A10 as (r2|r1)
+    
+    MOVLB 0xA
+    
+    ; move the contents of r2 to the high nibble and zero out the low nibble
+    SWAPF 0xA01, F, 1
+    MOVLW H'F0'
+    ANDWF 0xA01, F, 1
+    
+    ; zero out the low nibble of r1
+    MOVLW H'0F'
+    ANDWF 0xA00, W, 1
+    
+    ; combine r2 and r1 and move it to the output
+    IORWF 0xA01, W, 1
+    MOVWF 0xA10, 1
+    RETURN
+
+    
+UNPACK
+    ; Reads one double digit number stored in an 8-bit register, and
+    ; unpacks it into 2 single digit numbers stored in two 8-bit registers.
+    ;
+    ; Input at 0x0A10 as (r2|r1)
+    ; Output at 0x0A00 (r1) and 0x0A01 (r2)
+    
+    MOVLB 0xA
+    
+    MOVLW H'F0'
+    ANDWF 0xA10, W, 1
+    MOVWF 0xA01, 1
+    SWAPF 0xA01, F, 1
+    
+    MOVLW H'0F'
+    ANDWF 0xA10, W, 1
+    MOVWF 0xA00, 1
+    
+    RETURN
+    
+    
+BLK_CNT
+    ; Compares two 4-digit codes, each stored in an unpacked form in
+    ; 4 8-bit registers, and find the number of exact matches between the two. 
+    ; The upper nibbles of all registers hold the value 0H.
+    ;
+    ; Since WT_CNT will be called afterwards, any matches will have a flag set
+    ; in bit 7 to prevent a false positives for whites
+    ;
+    ; Input at 0xA00 - 0xA03 (r1 to r4 for CODE)
+    ;          0xA04 - 0xA07 (r5 to r8 for GUESS)
+    ; Output at 0xA10
+    
+    MOVLB 0xA
+    
+    CLRF 0xA10, 1
+    
+BLK_CNT_1
+    MOVF 0xA00, W, 1
+    CPFSEQ 0xA04, 1 ; check if CODE == GUESS
+    BRA BLK_CNT_2 ; if not, check the next row
+    
+    INCF 0xA10, F, 1 ; if so, increment the count and flag the values
+    BSF 0xA00, 7, 1
+    BSF 0xA04, 7, 1
+    
+BLK_CNT_2
+    MOVF 0xA01, W, 1
+    CPFSEQ 0xA05, 1
+    BRA BLK_CNT_3
+    
+    INCF 0xA10, F, 1
+    BSF 0xA01, 7, 1
+    BSF 0xA05, 7, 1
+    
+BLK_CNT_3
+    MOVF 0xA02, W, 1
+    CPFSEQ 0xA06, 1
+    BRA BLK_CNT_4
+    
+    INCF 0xA10, F, 1
+    BSF 0xA02, 7, 1
+    BSF 0xA06, 7, 1
+    
+BLK_CNT_4
+    MOVF 0xA03, W, 1
+    CPFSEQ 0xA07, 1
+    RETURN
+    
+    INCF 0xA10, F, 1
+    BSF 0xA03, 7, 1
+    BSF 0xA07, 7, 1
+
+BLK_CNT_END
+    RETURN
+    
+    
+WT_CNT
+    ; Compares two 4-digit codes, each stored in an unpacked form in 
+    ; 4 8-bit registers, and find the number of color matches between the two 
+    ; that are not considered in exact matches. The upper nibbles of all 
+    ; registers hold the value 0H. You need to be careful of double counting.
+    ;
+    ; Input at 0x0A00 - 0xA003 (r1 to r4 for CODE)
+    ;          0x0A04 - 0xA007 (r5 to r8 for GUESS)
+    ; Output at 0x0A10
+    
+    MOVLB 0xA
+    
+    CLRF 0xA10, 1 ; white count
+
+    MOVLW 4H ; outer loop counter
+    MOVWF 0xA20, 1
+    
+    MOVLW 4H ; inner loop counter
+    MOVWF 0xA21, 1
+    
+    LFSR 0, 0xA00
+    LFSR 1, 0xA04
+    
+WT_CNT_LOOP_1
+    ; skip this iteration if this code has been counted as a black
+    BTFSS INDF0, 7
+    
+    CALL WT_CNT_LOOP_2
+    
+    DCFSNZ 0xA20, F, 1
+    RETURN
+    
+    LFSR 1, 0xA04; reset guess pointer
+    
+    MOVLW 4H ; reset inner loop counter
+    MOVWF 0xA21, 1
+    
+    INCF FSR0L
+    BRA WT_CNT_LOOP_1
+    
+WT_CNT_LOOP_2
+    ; skip this iteration if this guess has been counted as a black
+    BTFSS INDF1, 7
+    CALL WT_CNT_CMP
+   
+    DCFSNZ 0xA21, F, 1
+    RETURN
+    
+    INCF FSR1L
+    BRA WT_CNT_LOOP_2
+    
+WT_CNT_CMP
+    ; skip this iteration if we are matching the same row
+    MOVF 0xA20, W, 1
+    SUBWF 0xA21, W, 1
+    BZ WT_CNT_CMP_END
+    
+    ; compare
+    MOVF INDF0, W
+    CPFSEQ INDF1
+    BRA WT_CNT_CMP_END
+    INCF 0xA10, F, 1
+    BSF INDF0, 7
+    BSF INDF1, 7
+    
+WT_CNT_CMP_END
+    RETURN
+    
+    
+RNG
+; randomizes a code until user presses SW2
+; Outputs to 0xA00-0xA03
+CPURAND0 EQU 0xA00
+CPURAND1 EQU 0xA01
+CPURAND2 EQU 0xA02
+CPURAND3 EQU 0xA03
+ 
+    MOVLB 0xA
+    
+    ;initialize counters to 5
+    MOVLW 5H
+    MOVWF CPURAND0, 1
+    MOVLW 5H
+    MOVWF CPURAND1, 1
+    MOVLW 5H
+    MOVWF CPURAND2, 1
+    MOVLW 5H
+    MOVWF CPURAND3, 1
+    BRA RCOUNT3 ; RCOUNT3 starts counting down the LSB
+    
+RCOUNT0 ; RESET bit 1
+    MOVLW 5H
+    MOVWF CPURAND1, 1
+RCOUNT1 ; RESET bit 2
+    MOVLW 5H
+    MOVWF CPURAND2, 1
+RCOUNT2 ; RESET bit 3
+    MOVLW 5H
+    MOVWF CPURAND3, 1
+RCOUNT3
+    ; check if SW2 is pressed
+    BTFSS PORTA, 4
+    BRA RNG_WAIT
+    
+    DECFSZ CPURAND3, 1
+    BRA RCOUNT3
+    ; R3 is 0
+    DECFSZ CPURAND2, 1
+    BRA RCOUNT2
+    ; R3 is 0 and R2 = 0
+    DECFSZ CPURAND1, 1
+    BRA RCOUNT1
+    ; R3 is 0 and R2 = 0 and R1 = 0
+    DECFSZ CPURAND0, 1
+    BRA RCOUNT0
+    ; R3 is 0 and R2 = 0 and R1 = 0 and R0 = 0
+    MOVLW 5H ; reset MSB
+    MOVWF CPURAND0, 1
+    BRA RCOUNT0
+ 
+RNG_WAIT
+    BTFSS PORTA, 4
+    BRA $-2
+    RETURN
+ 
+
+;*******************************************************************************
+; I/O SUBROUTINES
+;*******************************************************************************
+WAIT_BP
+    ; waits for a button press and release and returns which button got pushed
+    ; output at 0xA00, 0 if SW2, 1 if SW3
+    ; SW2 = RA4 = PORTA, 4
+    ; SW3 = RB0 = PORTB, 0
+;    BTFSS	PORTA, 4
+;    BRA		WT_RELEASE1
+;    BRA    	WT_PRESS1
+;    
+;    WT_RELEASE1    
+;    BTFSS	PORTA,4
+;    BRA		$-2
+    
+    
+    
+ENTER_GUESS
+    ; prompts user for a guess and stores it at 0xA00 - 0xA03
+    
+    
+; *****************  Low-Level LCD Routines  *******************
+#Include LCD_p18LCD_Subs_New.asm
+  ; Contains:
 ; --- LCDInit:		>> initialize
 ; --- LCDLine_1:	>> Cursor to Line_1
 ; --- LCDLine_2:	>> Cursor to to Line_2
 ; --- i_write:  	>> instruction write
 ; --- d_write:		>> data        write
- 
-    GOTO $                          ; loop forever     
     END
